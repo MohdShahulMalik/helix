737a260def36d8c4cd3e5fda66d6930da3ab6088
MohdShahulMalik <MohdShahulMalik@users.noreply.github.com>
2026-07-14 18:28:37 +0000

    deploy: 00678f9c4804937d7e45c280cefa4b5598f9533f

--- master/searcher-09f2665d.js
+++ master/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e027adc7.js',
+                path_to_root + 'searchindex-5ea43e44.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
